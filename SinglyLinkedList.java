b524aaccaa5ae4da0660c52f97c40cb06e10e9b8
awevishnu <awevishnu@gmail.com>
2020-10-23 20:20:45 +0530

    Add files via upload
    SinglyLinkedList Example
    Create SIngly Linked List
    Display Elements
    Length of the LinkedList
    Insert First
    Insert Last

---
+++ SinglyLinkedList.java
@@ -0,0 +1,60 @@
+package com.ds.example;
+
+public class SinglyLinkedList {
+    private ListNode head;
+    private static class ListNode
+    {
+      private int data;
+      private ListNode next;
+      public ListNode(int data)
+        {
+            this.data=data;
+            this.next=null;
+        }
+
+    }
+// traverse the linkedlist
+    public void display()
+    {
+        ListNode current=head;
+        while(current!=null)
+        {
+            System.out.print(current.data +"-->");
+            current=current.next;
+        }
+        System.out.println("null");
+    }
+    public int length()
+    {
+        if(head==null)
+        {
+            return 0;
+        }
+        int count=0;
+        ListNode current=head;
+        while(current!=null)
+        {
+            count++;
+            current=current.next;
+        }
+        return count;
+
+    }
+    public static void main(String[] args) {
+        SinglyLinkedList sll=new SinglyLinkedList();
+        sll.head=new ListNode(10);
+        ListNode second=new ListNode(1);
+        ListNode third=new ListNode(8);
+        ListNode fourth=new ListNode(11);
+        // connect together to form a chain
+        sll.head.next=second;// 10 --> 1
+        second.next=third;// 10 --> 1 --> 8
+        third.next=fourth; // 10 --> 1 --> 8 --> 11 --> null
+        sll.display();
+        System.out.println(sll.length());
+    }
+}
+    /*Time complexity of display method - O(n)
+     Space complexity of display method - O(1)
+     Time complexity of length method is O(n)
+     Space complexity of length method is O(1)*/
---
+++ SinglyLinkedList2.java
@@ -0,0 +1,91 @@
+package com.ds.example;
+
+public class SinglyLinkedList2 {
+
+    private SinglyLinkedList2.ListNode head;
+    private static class ListNode
+    {
+        private int data;
+        private SinglyLinkedList2.ListNode next;
+        public ListNode(int data)
+        {
+            this.data=data;
+            this.next=null;
+        }
+
+    }
+    // traverse the linkedlist
+    public void display()
+    {
+        SinglyLinkedList2.ListNode current=head;
+        while(current!=null)
+        {
+            System.out.print(current.data +"-->");
+            current=current.next;
+        }
+        System.out.println("null");
+    }
+    public int length()
+    {
+        if(head==null)
+        {
+            return 0;
+        }
+        int count=0;
+        SinglyLinkedList2.ListNode current=head;
+        while(current!=null)
+        {
+            count++;
+            current=current.next;
+        }
+        return count;
+
+    }
+    //inserting element at the begining of the linkedlist
+    public void insertFirst(int value)
+    {
+        ListNode newNode=new ListNode(value);
+        newNode.next=head;
+        head=newNode;
+    }
+    //inserting elements into last
+    public void insertLast(int value)
+    {
+        ListNode newNode=new ListNode(value);
+        if(head==null)
+        {
+            head=newNode;
+            return;
+        }
+        ListNode current=head;
+        while(null!=current.next)
+        {
+            current=current.next;
+        }
+        current.next=newNode;
+
+    }
+
+    public static void main(String[] args) {
+        SinglyLinkedList2 sll=new SinglyLinkedList2();
+        sll.insertFirst(11);
+        sll.insertFirst(8);
+        sll.insertFirst(2);
+        sll.display();
+        SinglyLinkedList2 sll2=new SinglyLinkedList2();
+        sll2.insertLast(2);
+        sll2.insertLast(3);
+        sll2.insertLast(4);
+        sll2.display();
+
+    }
+}
+/*
+Time complexity of insertFirst method is O(1)
+
+Space complexity of insertFirst method is O(1)
+
+Time complexity of insertLast method is O(n)
+
+Space complexity of insertLast method is O(1)
+ */
